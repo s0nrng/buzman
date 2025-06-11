29f9d1bb8918d223da4e76b2f5521a48a34662a8
Son Nguyen Dang <ndangson02@gmail.com>
2025-06-11 17:56:20 +0700

    second commit

---
+++ src/Components/order_management/new_order/CustomerTab.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function CustomerTab(){
+    const mainStyle = {
+        backgroundColor: 'white',
+        height: '100%',
+        marginRight: '3%',
+        width: '35%',
+        boxShadow: '0px 0px 10px 0.3px rgba(0, 0, 0, 0.2)',
+        borderRadius: 10
+    }
+
+    return (
+        <div style={mainStyle}></div>
+    );
+}
+
+export default CustomerTab;
--- src/Components/order_management/new_order/NewOrderTab.js
+++ src/Components/order_management/new_order/NewOrderTab.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import CustomerTab from './CustomerTab';
 
 function NewOrderTab(){
+    const mainStyle = {
+        marginTop: '1%',
+        width: '100%',
+        height: '70%',
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'row-reverse'
+    }
+
     return (
-        <div></div>
+        <div style={mainStyle}>
+            <CustomerTab/>
+        </div>
     );
 }
 
